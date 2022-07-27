b3ac9574ce20efdd522ffd9e50bb1c7fe9e792dd
Jay-snow <marcusjsnow1@gmail.com>
2022-07-27 15:53:17 -0400

    almost fully functional

--- app.js
+++ app.js
@@ -8,38 +8,86 @@ const api_url =
 class StartButton extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { liked: false, lat: 0, lon: 0, location: '', temp: 0, feels_like: 0
-     };
+    this.state = {
+      liked: false,
+      lat: 0,
+      lon: 0,
+      location: "",
+      temp: 0,
+      feels_like: 0,
+      clouds: 0,
+      humidity: 0,
+      wind_speed: 0,
+      wind_degree: 0,
+      weather_main: "",
+      weather_description: "",
+      sunrise: 0,
+      sunset: 0,
+      visibility: 0,
+    };
   }
 
+  //Fetchs long and lat from navigator. Set inside a promise so we can make it asynchronous later.
   getLongAndLat() {
     return new Promise((resolve, reject) =>
       navigator.geolocation.getCurrentPosition(resolve, reject)
     );
   }
+  setCallState(response) {
+    console.log(response);
+    //Set the state for the app
+    this.setState({ location: response.data.name });
+    this.setState({ temp: Math.round(response.data.main.temp) });
+    this.setState({
+      feels_like: Math.round(response.data.main.feels_like),
+    });
+    this.setState({ clouds: response.data.clouds.all });
 
-//   //callback func for navigator.getCurrentPosition
-//   getLocation = async (pos) => {
-//     console.log("begin geo");
-//     //Get the coords from the navigator
-//     const crd = pos.coords;
+    this.setState({ wind_speed: Math.round(response.data.wind.speed) });
+    this.setState({ wind_degree: response.data.wind.deg });
 
-//     //Set the state. Round the lat and long, as I'm not sure if this API accepts long floats.
-//     this.setState({ lat: Math.round(crd.latitude) });
-//     this.setState({ lon: Math.round(crd.longitude) });
+    this.setState({ weather_main: response.data.weather[0].main });
+    this.setState({
+      weather_description: response.data.weather[0].description,
+    });
 
-//     console.log(this.state.lat);
-//   };
+    this.setState({ sunrise: this.convertTime(response.data.sys.sunrise) });
+    this.setState({ sunset: this.convertTime(response.data.sys.sunset) });
+
+    this.setState({ visibility: this.convertKMtoMiles(response.data.visibility) });
+  }
 
   async callAPI() {
+    const user_latitude = localStorage.getItem("latitude");
+    const user_longitude = localStorage.getItem("longitude");
+
+    //Attempt to grab the API if it's been saved.
+
+    if (user_latitude && user_longitude) {
+      //Build the needed URL query
+      let url_query = `https://api.openweathermap.org/data/2.5/weather?lat=${user_latitude}&lon=${user_longitude}&units=imperial&appid=bc29f70f91ce3ca5a066b994763e3502`;
+
+      //Make the axios call
+      axios.get(url_query).then(
+        (response) => {
+          this.setCallState(response);
+        },
+        (error) => {
+          console.log(error);
+        }
+      );
+    }
+
     //Grabs the current location and stores it in state.
     //We need to wait until the coords have been grabbed before making the query.
     // await navigator.geolocation.getCurrentPosition(await this.getLocation);
 
-    let position = await this.getLongAndLat()
+    let position = await this.getLongAndLat();
 
+    //Save local storage
 
-
+    localStorage.setItem("latitude", position.coords.latitude);
+    localStorage.setItem("longitude", position.coords.longitude);
 
     //Build the needed URL query
     let url_query = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&units=imperial&appid=bc29f70f91ce3ca5a066b994763e3502`;
@@ -47,19 +95,50 @@ class StartButton extends React.Component {
     //Make the axios call
     axios.get(url_query).then(
       (response) => {
-        console.log(response)
-        //Set the state for the app
-        this.setState({location: response.data.name})
-        this.setState({temp: response.data.main.temp})
-        this.setState({feels_like: response.data.main.feels_like})
-
-
-
+        this.setCallState(response);
       },
       (error) => {
         console.log(error);
       }
     );
+  }
+
+  convertKMtoMiles(kilometers){
+    
+    return (kilometers / 1000)
+  }
+
+  convertTime(time) {
+    let converted_time = new Date(time * 1000);
+    return converted_time.toLocaleTimeString("en-us", {
+      hour: "2-digit",
+      minute: "2-digit",
+    });
+  }
+
+  componentDidMount() {
+    console.log("mounted!");
+    //Call API needs to store lat and long in localstorage
+    //Use local storage if its there
+    this.callAPI();
+  }
+
+  cloudStatusIcon() {
+    if (this.state.clouds > 75) {
+      return <span className="material-symbols-outlined md-60">cloud</span>;
+    } else {
+      return (
+        <span className="material-symbols-outlined md-60 md-light">sunny</span>
+      );
+    }
+  }
+
+  cloudStatusText() {
+    if (this.state.clouds > 75) {
+      return <span>Cloudy</span>;
+    } else {
+      return <span>Sunny</span>;
+    }
   }
 
   render() {
@@ -68,25 +147,67 @@ class StartButton extends React.Component {
     }
 
     return (
-      <div>
-        {/* <button onClick={() => this.setState({ liked: true })}>Hi!</button> */}
-        <button onClick={() => this.callAPI()}>Call API!</button>
+      <div className="fadeInUp">
+        {/* Location Icon and Area */}
+        <h2 className="text-icon-wrapper">
+          <span className="material-symbols-outlined">location_on</span>
+          {this.state.location}
+        </h2>
+        {/* Time and Date */}
+        <p className="text-center" style={{ marginBottom: 0 }}>
+          <span style={{ marginRight: "10px" }}>
+            {new Date().toLocaleString("en-us", {
+              weekday: "short",
+              month: "long",
+              day: "numeric",
+            })}
+          </span>
 
-        <p>{new Date().toLocaleString()}</p>
-        <h2>{this.state.location}</h2>
-        <h2>{this.state.temp} Degrees</h2>
-        <h2>Feels like {this.state.feels_like}</h2>
-        <h2>Cloudy</h2>
+          <span>
+            {new Date().toLocaleTimeString([], {
+              hour: "2-digit",
+              minute: "2-digit",
+            })}
+          </span>
+        </p>
+        {/* Actual Temperature */}
+        <p style={{ margin: 0 }} className="text-icon-wrapper">
+          {this.cloudStatusIcon()}
+          <span id="temperature">{this.state.temp}</span>{" "}
+          <span id="degree">
+            <sup>&#176;</sup>
+          </span>
+        </p>
+        {/* Weather Description */}
+        <h2 className="text-center" style={{ textTransform: "capitalize", margin: 0 }}>
+          {this.state.weather_description}
+        </h2>
+        {/* todo Need to make cloudy status conditional */}
+        <section style={{ display: "flex", justifyContent: "space-between" }}>
+          <p>Feels Like: {this.state.feels_like}</p>
+          <p>Wind: {this.state.wind_speed}</p>
+          <p>Visibility: : {this.state.visibility}</p>
+        </section>
+        <hr></hr>
+        <section style={{justifyContent: 'left'}}>
+          <div>
+            <h4>Sunrise:</h4>
+            <div className="sun-icon-wrapper ">
+              <span className="material-symbols-outlined md-48 mr-1">brightness_5</span>{" "}
+              <span style={{fontSize: '1.75rem'}}>{this.state.sunrise}</span>
+            </div>
+          </div>
+          <div>
+            <h4>Sunset</h4>
+            <div className="sun-icon-wrapper">
+              <span className="material-symbols-outlined md-48 mr-1">wb_twilight</span>{" "}
+              <span style={{fontSize: '1.75rem'}}>{this.state.sunset}</span>
+            </div>
+            
+          </div>
+        </section>
       </div>
     );
-
-    // return e(
-    //   'button',
-    // //   { onClick: () => this.setState({ liked: true }) },
-    //   { onClick: () => this.callAPI() },
-
-    //   'Like'
-    // );
   }
 }
 
--- readme.md
+++ readme.md
@@ -2,9 +2,21 @@
 
 1. Look at some weather app templates for inspiration [x]
 2. Design base level of weather app
+  A. Clean up current CSS, convert long styles into classes [x]
+  B. Pick out a font to use for the app [x]
+  C. Work on spacing/sizing for text [x]
+  D. Seperate logic for cloudStatus. Make one return text and one return an icon. [x]
+  E. Do something about the place-holder background. [x]
+
 3. Introduce REACTjs [x]
-4. Look more into promises. Use the navigator to practice and understand.
+4. Convert sunrise and sunshine to time [x]
+5. Externalize CSS. Maybe this is time to mess around with sass?
+
 
 # INfo
 
 https://api.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=bc29f70f91ce3ca5a066b994763e3502
+
+# Post Project To Do
+1. Look more into promises. Use the navigator to practice and understand.
+2. Converting time from (UTC?) in javascript
